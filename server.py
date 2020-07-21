ff16901eebe76a656a31e7a402944b0d94296759
Sebastien Le Digabel <sebastien.ledigabel@skyscanner.net>
2020-07-21 12:48:40 +0100

    Updating app and bumping version

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-DEFAULT_REDIS_SERVER = 'localhost'
-DEFAULT_REDIS_PORT = '6379'
+DEFAULT_REDIS_SERVER = "localhost"
+DEFAULT_REDIS_PORT = "6379"
 global redis, local_count
 
 
@@ -14,22 +14,23 @@ def hello():
     global local_count
     try:
         local_count += 1
-        count = redis.get('global_connections_count')
-        count = 1 if count is None else int(count)+1
+        count = redis.get("global_connections_count")
+        count = 1 if count is None else int(count) + 1
         redis.incr("global_connections_count")
     except RedisError as e:
-        return 'Redis error: {}'.format(e), 500
+        return "Redis error: {}".format(e), 500
 
     return "<html><body>HOST: {}\n<br/>\n\
         Count: {}\n<br/>\n\
         Number of apps: {}\n<br>\n\
         Page visited: {} times\n<br/>\
-        <img src='https://i.ytimg.com/vi/L_I-26YSHrI/maxresdefault.jpg'/>\
+        <img src='https://media.giphy.com/media/u18KbOWs65HFK/giphy.gif'/>\
         <br/></body></html>".format(
-            environ.get("HOSTNAME", "hostname"),
-            local_count,
-            len(redis.client_list()),
-            count)
+        environ.get("HOSTNAME", "hostname"),
+        local_count,
+        len(redis.client_list()),
+        count,
+    )
 
 
 @app.route("/healthcheck")
@@ -38,16 +39,16 @@ def healthcheck():
         redis.ping()
     except RedisError as e:
         return f"App cannot connect to Redis:{e}", 500
-    return 'App is healthy'
+    return "App is healthy"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    REDIS_SERVER = environ.get('REDIS_SERVER', DEFAULT_REDIS_SERVER)
-    REDIS_PORT = environ.get('REDIS_PORT', DEFAULT_REDIS_PORT)
+    REDIS_SERVER = environ.get("REDIS_SERVER", DEFAULT_REDIS_SERVER)
+    REDIS_PORT = environ.get("REDIS_PORT", DEFAULT_REDIS_PORT)
 
-    SERVER = '0.0.0.0'
-    PORT = environ.get('PORT', 5000)
+    SERVER = "0.0.0.0"
+    PORT = environ.get("PORT", 5000)
     redis = Redis(host=REDIS_SERVER, port=REDIS_PORT)
 
     local_count = 0
@@ -55,6 +56,5 @@ if __name__ == '__main__':
     # redis.set('global_connections_count', 0)
 
     if not redis.ping():
-        print("Could not connect to REDIS @ {}:{}".format(
-            REDIS_SERVER, REDIS_PORT))
+        print("Could not connect to REDIS @ {}:{}".format(REDIS_SERVER, REDIS_PORT))
     app.run(host=SERVER, port=PORT)
